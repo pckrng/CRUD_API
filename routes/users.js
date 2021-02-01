82ebb4642260f39b58f7b8bcdf505125be28d670
pckrng <pckrng@outlook.com>
2021-02-01 02:19:31 +0000

    pre-patch

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "Joe",
-        lastName: "Pickering",
-        age: 23
-    }
-]
+let users = []
 
 router.get('/', (req, res) => {
 
@@ -24,6 +18,21 @@ router.post('/', (req, res) => {
     users.push({ ...user, id: uuidv4() });
 
     res.send(`User ${user.firstName} has been added!`);
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser)
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id !== id );
+
+    res.send(`User ${id} has been deleted`);
 });
 
 export default router;
